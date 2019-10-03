a0e2687cd333017532eeee6e0ecf4ecc46b67633
sumaks-xr <suman.singhxr@gmail.com>
2019-10-03 08:24:15 +0530

    first commit

---
+++ ARSketchBook/Assets/DataFiles/MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float xvalue;
+    public float yvalue;
+    public float zvalue;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(xvalue, yvalue, zvalue) * Time.deltaTime);
+    }
+}
---
+++ ARSketchBook/Assets/DataFiles/ShipMoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0.5f, 0,0.5f) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime);
+    }
+}
